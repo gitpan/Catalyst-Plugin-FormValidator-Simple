5e1c5279de0b84f058dde16eb9161074e5862c43
Lyo Kato <lyokato@cpan.org>
2005-11-17 14:33:27 +0000

    Import of LYOKATO/Catalyst-Plugin-FormValidator-Simple-0.06 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-FormValidator-Simple
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         LYOKATO/Catalyst-Plugin-FormValidator-Simple-0.06.tar.gz
    gitpan-cpan-author:       LYOKATO
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/FormValidator/Simple.pm
+++ lib/Catalyst/Plugin/FormValidator/Simple.pm
@@ -6,21 +6,24 @@ use base qw/Catalyst::Plugin::FormValidator/;
 use NEXT;
 require FormValidator::Simple;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub setup {
     my $self = shift;
-    $self->NEXT::setup(@_);
+    $self->NEXT::ACTUAL::setup(@_);
     my $setting = $self->config->{validator};
     my $plugins = $setting && exists $setting->{plugins}
         ? $setting->{plugins}
         : [];
     FormValidator::Simple->import(@$plugins);
+    if ( $setting && exists $setting->{messages} ) {
+    FormValidator::Simple->set_messages( $setting->{messages} );
+    }
 }
 
 sub prepare {
     my $c = shift;
-    $c = $c->NEXT::prepare(@_);
+    $c = $c->NEXT::ACTUAL::prepare(@_);
     my $setting = $c->config->{validator};
     my $options = $setting && exists $setting->{options}
     	? $setting->{options}
@@ -91,7 +94,7 @@ in your controller
 
         }
 
-        if ( $c->form->has_missing || $c->form->has_invalid ) {
+        if ( $c->form->has_error ) {
             
             if ( $c->form->missing('param1') ) {
                 ...
@@ -114,6 +117,309 @@ This plugin allows you to validate request parameters with FormValidator::Simple
 See L<FormValidator::Simple> for more information.
 
 This behaves like as L<Catalyst::Plugin::FormValidator>.
+
+=head1 CONFIGURATION
+
+set config with 'validator' key.
+
+    MyApp->config->{validator} = { ... };
+
+or
+
+    MyApp->config(
+        validator => { ... },
+    );
+
+=head2 PLUGINS
+
+If you want to use come plugins for FormValidator::Simple, you can set like follows.
+
+    MyApp->config(
+        validator => {
+            plugins => [qw/Japanese CreditCard DBIC::Unique/],
+        },
+    );
+
+In this example, FormValidator::Simple::Plugin::Japanese, FormValidator::Simple::Plugin::CreditCard,
+and FormValidator::Simple::Plugin::DBIC::Unique are loaded.
+
+=head2 OPTIONS
+
+When you set some options needed by specific validations, do like this.
+
+    MyApp->config(
+        validator => {
+            plugins => [qw/Japanese CreditCard DBIC::Unique/],
+            options => {
+                charset => 'euc',
+                dbic_base_class => 'MyApp::Model::DBIC',
+            },
+        },
+    );
+
+'charset' is necessary for Plugin::Japanese, and 'dbic_cbase_class' is used in Plugin::DBIC::Unique.
+
+=head1 VALIDATION
+
+use 'form' method, see L<FormValidator::Simple> in detail.
+
+    sub do_add : Local {
+        my ( $self, $c ) = @_;
+
+        # execute validation.
+        $c->form(
+            name  => [qw/NOT_BLANK ASCII/,       [qw/LENGTH 0 20/] ],
+            email => [qw/NOT_BLANK EMAIL_LOOSE/, [qw/LENGTH 0 20/] ],
+            { unique => [qw/name email/] } => [qw/DBIC_UNIQUE User name email/],
+        );
+
+        if ( ... ) {
+
+            # execute validation one more time in specific condition.
+            $c->form(
+                ...                 
+            );
+
+        }
+
+        # See Catalyst::Plugin::RequestToken for '$c->validate_token'
+        if ( $c->validate_token ) {
+
+            # you can force to set invalid data.
+            $c->set_invalid_form( token => 'TOKEN' );
+
+        }
+
+        # check result.
+        # you can pick up result-object with 'form' method
+
+        my $result = $c->form;
+
+        if ( $result->has_error ) {
+
+        # this is same as
+        # if ( $result->has_missing or $result->has_invalid )
+
+            $c->detach('add');
+
+        }
+
+    }
+
+=head1 HANDLING SUCCESSFUL RESULT
+
+After it passes all validations, you may wanna put input-data into database.
+It's a elegant way to use [ L<Class::DBI> and L<Class::DBI::FromForm> ] or [ L<DBIx::Class> and L<DBIx::Class::WebForm> ].
+
+    sub do_add : Local {
+        my ( $self, $c ) = @_;
+
+        $c->form(
+            name  => [qw/NOT_BLANK/],
+            email => [qw/NOT_BLANK/],
+        );
+
+        my $result = $c->form;
+        if ( $result->has_error ) {
+            $c->detach('add');
+        }
+
+        my $user = MyProj::Model::DBIC::User->create_from_form($result);
+        
+        # this behaves like this...
+        # MyProj::Model::DBIC::User->create({
+        #    name  => $result->valid('name'),
+        #    email => $result->valid('email'),
+        # });
+        #
+        # if the key exists as the table's column, set the value with 'valid'
+    }
+
+Here, I explain about 'valid' method. If the value indicated with key-name passes validations,
+You can get the data with 'valid',
+
+    my $result = $c->form(
+        name  => [qw/NOT_BLANK/],
+        email => [qw/NOT_BLANK/],
+    ); 
+
+    print $result->valid('name');
+
+    print $result->valid('email');
+
+But, this is for only single key validation normally.
+
+    my $result = $c->form(
+        name => [qw/NOT_BLANK/], # single key validation
+        { mail_dup => [qw/email email2/] } => ['DUPLICATION'] # multiple keys one
+    );
+
+    print $result->valid('name'); # print out the value of 'name'
+
+    print $result->valid('mail_dup'); # no value.
+
+There are exceptions. These are 'DATETIME', 'DATE'.
+
+    my $result = $c->form(
+        { created_on => [qw/created_year created_month created_day/] }
+        =>
+        [qw/DATETIME/],
+    );
+
+    print $result->valid('created_on'); #print out datetime string like "2005-11-23 00:00:00".
+
+If you set some class around datetime in configuration. It returns object of the class you indicate.
+You can choose from L<Time::Piece> and L<DateTime>. For example...
+
+    MyApp->config(
+        validator => {
+            plugins => [...],
+            options => {
+                datetime_class => 'Time::Piece',
+            },
+        },
+    );
+
+or
+
+    MyApp->config(
+        validator => {
+            plugins => [...],
+            options => {
+                datetime_class => 'DateTime',
+                time_zone      => 'Asia/Tokyo',
+            },
+        },
+    );
+
+then
+
+    my $result = $c->form(
+        { created_on => [qw/created_year created_month created_day/] }
+        =>
+        [qw/DATETIME/],
+    );
+
+    my $dt = $result->valid('created_on');
+
+    print $dt->ymd;
+
+    MyProj::Model::CDBI::User->create_from_form($result);
+
+This may be useful when you define 'has_a' relation for datetime columns.
+For example, in your table class inherits 'Class::DBI'
+
+    __PACKAGE__->has_a( created_on => 'DateTime',
+        inflate => ...,
+        deflate => ...,
+    );
+
+And see also L<Class::DBI::Plugin::TimePiece>, L<Class::DBI::Plugin::DateTime>.
+
+=head1 MESSAGE HANDLING
+
+in template file, you can handle it in detail.
+
+    [% IF c.form.has_error %]
+    <p>Input Error</p>
+    <ul>
+    [% IF c.form.missing('name') %]
+    <li>input name!</li>
+    [% END %]
+    [% IF c.form.invalid('name') %]
+    <li>name is wrong</li>
+    [% END %]
+    [% IF c.form.invalid('name', 'ASCII') %]
+    <li>input name with ascii code.</li>
+    [% END %]
+    [% IF c.form.invalid('name', 'LENGTH') %]
+    <li>wrong length for name.</li>
+    [% END %]
+    </ul>
+    [% END %]
+
+or, make it more easy.
+
+    [% IF c.form.has_error %]
+    <p>Input Error</p>
+    <ul>
+    [% FOREACH key IN c.form.error %]
+        [% FOREACH type IN c.form.error(key) %]
+        <li>Invalid: [% key %] - [% type %]</li>
+        [% END %]
+    [% END %]
+    </li>
+    [% END %]
+
+And you can also use messages configuration as hash reference.
+
+    MyApp->config(
+        validator => {
+            plugins  => [...],
+            messages => {
+                user => {
+                    name => {
+                        NOT_BLANK => 'Input name!',
+                        ASCII     => 'Input name with ascii code!',
+                    },
+                    email => {
+                        DEFAULT   => 'email is wrong.!',
+                        NOT_BLANK => 'input email.!'
+                    },
+                },
+                company => {
+                    name => {
+                        NOT_BLANK => 'Input name!',
+                    },
+                },
+            },
+        },
+    );
+
+or YAML file. set file name
+
+    MyApp->config(
+        validator => {
+            plugins  => [...],
+            messages => 'conf/messages.yml',
+        },
+    );
+
+and prepare yaml file like follows,
+
+    user:
+        name:
+            NOT_BLANK: Input name!
+            ASCII: Input name with ascii code!
+        email:
+            DEFAULT: Email is wrong!
+            NOT_BLANK: Input email!
+    company:
+        name:
+            NOT_BLANK: Input name!
+
+the format is...
+
+    Action1_Name:
+        Key1_Name:
+            Validation1_Name: Message
+            Validation2_Name: Message
+        Key2_Name:
+            Validation1_Name: Message
+    Action2_Name:
+        Key1_Name:
+            ...
+        
+After messages configuration, call messages() method from result-object.
+and set action-name as argument.
+
+    [% IF c.form.has_error %]
+    <ul>
+        [% FOREACH message IN c.form.messages('user') %]
+        <li>[% message %]</li>
+        [% END %]
+    </ul>
+    [% END %]
 
 =head1 SEE ALSO
 
